12c86fd9f5520d60377c662dc986f39ed09d124c
nzolotko <natalia.zolotko@grandparade.co.uk>
2018-11-23 17:52:15 +0100

    add client

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+import Launches from './components/Launches';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
+
+class App extends Component {
+  render() {
+
+    return (
+      <ApolloProvider client={client}>
+        <div className="container">
+          <h1>SpaceX</h1>
+        </div>
+        <Launches />
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
